b10ce4fb9ccf4725298aa495b28413b466a5274f
syabrinaa <92876433+syabrinaa@users.noreply.github.com>
2022-03-01 15:46:53 +0700

    Add files via upload

---
+++ programsederhana/programsederhana/MainForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ASUS
+ * Date: 01/03/2022
+ * Time: 15:02
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace programsederhana
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 20;
+			this.listBox1.Location = new System.Drawing.Point(96, 128);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(283, 104);
+			this.listBox1.TabIndex = 0;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(139, 75);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(195, 26);
+			this.textBox1.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(192, 30);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 23);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "List Belanja\r\n";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(354, 75);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(136, 47);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "Tambahkan\r\n";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(522, 342);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.listBox1);
+			this.Name = "MainForm";
+			this.Text = "programsederhana";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ programsederhana/programsederhana/MainForm.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ASUS
+ * Date: 01/03/2022
+ * Time: 15:02
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace programsederhana
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			listBox1.Items.Add(textBox1.Text);
+		}
+	}
+}
